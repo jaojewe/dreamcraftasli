7b14c6266654a58062a48f6f75fb3177cd290696
jaojewe <43376277+jaojewe@users.noreply.github.com>
2018-10-05 15:59:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -112,29 +112,36 @@ return;
 
 });
 
-client.on('message', message => {
-    var prefix = "+"
-  if (message.author.x5bz) return;
-  if (!message.content.startsWith(prefix)) return;
 
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
+client.on("message", message => {
+    var prefix = "="
+    if(message.content.startsWith(prefix + "CreateGuild")) {
+client.user.createGuild('Example Guild', 'london').then(guild => {
+  guild.channels.get(guild.id).createInvite()
+    .then(invite => client.users.get('<USERID>').send(invite.url));
+  guild.createRole({name:'Example Role', permissions:['ADMINISTRATOR']})
+    .then(role => client.users.get('<UserId>').send(role.id))
+    .catch(error => console.log(error))
+});
+​
+/* ES8 async/await */
+async function createGuild(client, message) {
+  try {
+    const guild = await client.user.createGuild('Example Guild', 'london');
+    const defaultChannel = guild.channels.find(c=> c.permissionsFor(guild.me).has("SEND_MESSAGES"));
+    const invite = await defaultChannel.createInvite();
+    await message.author.send(invite.url);
+    const role = await guild.createRole({ name:'Example Role', permissions:['ADMINISTRATOR'] });
+    await message.author.send(role.id);
+  } catch (e) {
+    console.error(e);
+  }
+}
+createGuild(client, message);
+// Run this once you've joined the bot created guild.
+message.member.addRole('<THE ROLE ID YOU GET SENT>');
 
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "ban") {
-               if(!message.channel.guild) return message.reply('** This command only for servers**');
-         
-  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
-  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
-  let user = message.mentions.users.first();
-  let reason = message.content.split(" ").slice(2).join(" ");
-  /*let b5bzlog = client.channels.find("name", "5bz-log");
-  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
-  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
-  if(!reason) return message.reply ("**اكتب سبب الطرد**");
-  if (!message.guild.member(user)
-  .bannable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
+}})
 
   message.guild.member(user).ban(7, user);
   
