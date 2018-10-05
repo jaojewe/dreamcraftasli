59cc1a56f04aae455b0883e6481fe6ea5af0cf3b
jaojewe <43376277+jaojewe@users.noreply.github.com>
2018-10-05 14:50:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,25 +28,6 @@ client.user.setGame(`DreamCraft`,"http://twitch.tv/S-F")
   console.log('')
 });
 
-const adminprefix = "+";
-const devs = ['428890200735875073'];
-    client.on('message', message => {
-        if (message.author.bot) return;
-        if (!message.content.startsWith(prefix)) return;
-      
-        let command = message.content.split(" ")[0];
-        command = command.slice(prefix.length);
-      
-      
-      let args = message.content.split(" ").slice(1);
-      let x = args.join(" ")
-        if(message.content.startsWith(adminprefix + 'say')) {
-            message.channel.send(''+x);
-                message.delete(999)
-        }
-        
-       
-      });
 
 
 client.on('message' , message => {
