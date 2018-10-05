28fbb8bfcf0e4ba137e05866b7c4c65857aad16e
jaojewe <43376277+jaojewe@users.noreply.github.com>
2018-10-05 15:02:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,31 +61,6 @@ client.on("message", message => {
 
 
 
-client.on('message', message => {
-    if (message.content === "+server") {
-        if (!message.channel.guild) return
-        var verificationLevel = message.guild.verificationLevel;
-        const verificationLevels = ['None','Low','Meduim','High','Extreme'];
-        var Y1 = message.guild.createdAt.getFullYear() - 2000
-        var M2 = message.guild.createdAt.getMonth()
-        var D3 = message.guild.createdAt.getDate()
-        const xNiTRoZ = new Discord.RichEmbed()
-         .setAuthor(message.author.username , message.author.avatarURL)
-         .setColor('RANDOM')
-         .setTimestamp()
-         .setTitle(message.guild.name,message.guild.iconURL)
-         .addField(':id: اي دي السيرفر',`${message.guild.id}`,true)
-         .addField(':date: أنشئت في',D3 + '.' + M2 + '.' + Y1,true)             
-         .addField(':crown: اونر السيرفر',`${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`)             
-         .addField(':busts_in_silhouette: الاعضاء ' + ` ${message.guild.memberCount} `,'Online '+`[ ${message.guild.members.filter(m=>m.presence.status == 'online','idle','dnd').size} ]`+ ','+'Offline '+`[ ${message.guild.members.filter(m=>m.presence.status == 'offline').size} ]`,true)
-         .addField(':speech_balloon: قنوات' +' '+message.guild.channels.size+' ',`Text [ ${message.guild.channels.filter(m => m.type === 'text').size} ]`+', '+`Voice [ ${message.guild.channels.filter(m => m.type === 'voice').size} ]`,true)
-         .addField(':earth_asia: الدوله',message.guild.region)
-         .addField(':ribbon: ايموجي السيرفر',`${message.guild.emojis.size}`,true)
-         .addField(':construction: مستوى التحقق',`${verificationLevels[message.guild.verificationLevel]}`,true)
-         .addField(':closed_lock_with_key: الرتب  '+message.guild.roles.size+' ','!')
-         message.channel.send({embed:xNiTRoZ});
-     }
-    });
 
 
 client.on('message', message => {
@@ -110,33 +85,6 @@ client.on('message', message => {
 
 
 
-
-client.on('message', message => {
-    if(message.content.includes('discord.gg/')) {
-        if(message.member.hasPermission('ADMINISTRATOR')) return;
-        message.delete();
-        message.guild.member(message.author).addRole(message.guild.roles.find(r => r.name === 'Muted'));
-        let embedP = new Discord.RichEmbed()
-        .setTitle('❌ | تمت معاقبتك')
-        .setAuthor(message.author.username, message.author.avatarURL)
-        .addField(`** لقد قمت بمخالفة قوانين السيرفر من خلال نشر روابط اضافة الى سيرفرات اخرى  **` , `**ملاحظة  : إن كآن هذآ الاسكات عن طريق الخطأ الرجاء التوجه والتكلم مع الادآرة**`)
-        .addField(`by`,`Hello,Im'iiLizx`)
-        .setColor('RED')
-        .setThumbnail(message.author.avatarURL)
-        .setFooter(`${message.guild.name} Server`, message.guild.iconURL)
-        
-        message.channel.send(embedP);
-    }
-});
-
-
-
-	client.on('message', msg => {
-  if (msg.content === 'fuck') {
-   msg.delete(30)
-    msg.reply('ممنوع');
-  }
-});
 
 
 
