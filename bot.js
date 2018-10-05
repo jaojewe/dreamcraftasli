d7908380d4e867c4cb29e9e632c6580dfbd4a971
jaojewe <43376277+jaojewe@users.noreply.github.com>
2018-10-05 14:48:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -231,55 +231,6 @@ client.on('message', message => {
 
 
 
-  
-  client.on('message',message =>{
-      var prefix = "+";
- if(message.content.split(' ')[0].toLowerCase() == prefix + 'myinv') {
-let guild = message.guild
-var codes = [""]
- var nul = 0
-      
-guild.fetchInvites()
-.then(invites => {
-invites.forEach(invite => {
-if (invite.inviter === message.author) {
-    nul+=invite.uses
-codes.push(`discord.gg/${invite.code}`)
-}
- 
-})
-  if (nul > 0) {
-      const e = new Discord.RichEmbed()
-      .addField(`${message.author.username}`, `لقد قمت بدعوة **${nul}** شخص`)
-      .setColor('#36393e')
-      message.channel.send(e)
-  }else {
-                       var embed = new Discord.RichEmbed()
-                        .setColor("#000000")
-                        .addField(`${message.author.username}`, `لم تقم بدعوة أي شخص لهذة السيرفر`)
-
-                       message.channel.send({ embed: embed });
-                        return;
-                    }
-}).then(m => {
-if (codes.length < 0) {
-    var embed = new Discord.RichEmbed()
-.setColor("#000000")
-.addField(`Your invites in ${message.guild.name}`, `You currently don't have any active invites! Please create an invite and start inviting, then you will be able to see your codes here!`)
-message.channel.send({ embed: embed });
-return;
-} else {
-    var embed = new Discord.RichEmbed()
-.setColor("#000000")
-.addField(`Your invites in ${message.guild.name}`, `Invites :\n${codes.join("\n")}`)
-.setColor('#36393e')
-message.channel.send({ embed: embed });
-return;
-}
-})
-}
-
-});
 
 
 client.on('message', message => {
@@ -363,53 +314,7 @@ message.channel.send(`**:white_check_mark: ${user.tag} kicked from the server ! 
 });
 
 
-client.on('message', message => {
-  if(message.content === '+support') {
-  const embed = new Discord.RichEmbed()
-  .setTitle('Click here')
-  .setURL('https://discord.gg/ySnVHNd ')
-  .setColor('RANDOM')
-  message.channel.send({embed: embed});
-  }
-});
-
  
- client.on('message' , message => {
-  var prefix = "+";
-  if(message.author.bot) return;
-  if(message.content.startsWith(prefix + "send")) {
-    let args = message.content.split(" ").slice(1);
-
-
-    let suggestmessage = args.join(" ").slice(22);
-    let suggestchannel = message.mentions.channels.first();
-
-    if (!suggestchannel) {
-        return message.reply("Please Mention the channel!")
-    }
-
-    if (!suggestmessage) {
-        return message.reply("Plase Give Text To send Channel!")
-    
-         
-    }
-     message.delete();
-suggestchannel.send("@everyone  `||` @here ");
-    let embed = new Discord.RichEmbed()
-        .addField("**Massge**", `${suggestmessage}`)
-        .setFooter(`by ${message.author.tag}`)
-        .setTimestamp()
-    suggestchannel.send({
-        embed
-    }).then(msg => {
-        msg.react("✅").then(r => msg.react("❎"))
-    });
-
-
-    message.reply(`Your message is sended.`).then(msg => msg.delete(1000));
-    return;
-}
-});
 
  
 client.on('message', message => {
