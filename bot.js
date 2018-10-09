c3fc37db17b4941e68544d50b641dbb7e6704be7
jaojewe <43376277+jaojewe@users.noreply.github.com>
2018-10-09 15:22:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '+'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`DreamCraft l +help`,"Playing")
+client.user.setGame(`DreamCraft l +help`,"playing")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
