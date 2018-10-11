2c305fb4d053ea04fb225ba1b8c9527e5c3ea514
jaojewe <43376277+jaojewe@users.noreply.github.com>
2018-10-11 15:25:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1835,6 +1835,45 @@ if (message.content.startsWith(prefix + 'clear')) {
 			  
 			  
 			 
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("463277981146808331");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
+ 
+ 
+ 
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("499935158443704330");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("463277981146808331");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;        
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
 
 
 
