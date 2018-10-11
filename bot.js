6228a4c7132db62072157eaf6575257846f6aa75
jaojewe <43376277+jaojewe@users.noreply.github.com>
2018-10-11 21:30:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1800,15 +1800,15 @@ client.on("guildMemberAdd", (member) => {
 const fs = require('fs'); // npm i fs
 const ms = require('ms'); // npm i ms
 const cool = [];
-hero.on('message',async message => {
+client.on('message',async message => {
   if(message.author.bot) return;
   if(message.channel.type === 'dm') return;
  
   const args = message.content.split(' ');
   const credits = require('./credits.json');
   const path = './credits.json';
-  const mention = message.mentions.users.first() || hero.users.get(args[1]) || message.author;
-  const mentionn = message.mentions.users.first() || hero.users.get(args[1]);
+  const mention = message.mentions.users.first() || client.users.get(args[1]) || message.author;
+  const mentionn = message.mentions.users.first() || client.users.get(args[1]);
   const author = message.author.id;
   const balance = args[2];
   const daily = Math.floor(Math.random() * 350) + 10;
