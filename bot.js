866f0e99c7bd26428abbbfef6ccebf2c5c0afbe5
jaojewe <43376277+jaojewe@users.noreply.github.com>
2018-10-11 15:09:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 const prefix = '+'
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`DreamCraft l +help`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
